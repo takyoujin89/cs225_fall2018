2066550fad50fba552fab9c325d8b85845357f4d
Wade Fagen-Ulmschneider <waf@illinois.edu>
2018-08-31 08:32:29 -0500

    mp1 release

---
+++ mp1/mp1.cpp
@@ -0,0 +1,20 @@
+#include "cs225/PNG.h"
+using cs225::PNG;
+
+#include "cs225/HSLAPixel.h"
+using cs225::HSLAPixel;
+
+#include <string>
+
+
+
+void rotate(std::string inputFile, std::string outputFile) {
+  // TODO: Part 2
+}
+
+PNG myArt(unsigned int width, unsigned int height) {
+  PNG png(width, height);
+  // TODO: Part 3
+
+  return png;
+}
