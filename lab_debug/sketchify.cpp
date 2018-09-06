1c7fe3c80bf452ca6e919e81059ad3b2c92f7be0
Austin Lee <sal3@siebl-0224-29.ews.illinois.edu>
2018-09-06 14:55:23 -0500

    lab 2

--- lab_debug/sketchify.cpp
+++ lab_debug/sketchify.cpp
@@ -13,39 +13,46 @@ PNG* setupOutput(unsigned w, unsigned h) {
 
 // Returns my favorite color
 HSLAPixel* myFavoriteColor(double hue) {
-  HSLAPixel p(hue, 0.8, 0.5);
-  return &p;
+  HSLAPixel *p = new HSLAPixel(hue, 0.8, 0.5);
+  return p;
 }
 
 void sketchify(std::string inputFile, std::string outputFile) {
     // Load in.png
-    PNG* original = NULL;
-
+    PNG* original= new PNG();
+cout << "Reached line 23" << endl;
     original->readFromFile(inputFile);
-    unsigned width = original->width();
-    unsigned height = original->height();
-
+    cout << "Reached line 25" << endl;
+    unsigned int width = original->width();
+    cout << width << endl;
+    unsigned int height = original->height();
+cout << height << endl;
     // Create out.png
-    PNG* output;
-    setupOutput(width, height);
+    PNG* output = setupOutput(width, height);
 
     // Load our favorite color to color the outline
-    HSLAPixel* myPixel = myFavoriteColor(-1);
+    HSLAPixel* myPixel = myFavoriteColor(360);
 
     // Go over the whole image, and if a pixel differs from that to its upper
     // left, color it my favorite color in the output
-    for (unsigned y = 1; 0 < y < height; y++) {
-        for (unsigned x = 1; 0 < x < width; x++) {
+   for (unsigned int y = 1; y< height; y++) {
+        for (unsigned int x = 1; x < width; x++) {
             // Calculate the pixel difference
+
             HSLAPixel& prev = original->getPixel(x - 1, y - 1);
+
             HSLAPixel& curr = original->getPixel(x, y);
+
             double diff = std::fabs(curr.h - prev.h);
+
 
             // If the pixel is an edge pixel,
             // color the output pixel with my favorite color
-            HSLAPixel currOutPixel = (*output).getPixel(x, y);
+            HSLAPixel &currOutPixel=(*output).getPixel(x, y);
+
             if (diff > 20) {
                 currOutPixel = *myPixel;
+
             }
         }
     }
