35059bad437c94d046c54f1f8fb2cde1ebfd086e
Wade Fagen-Ulmschneider <waf@illinois.edu>
2018-09-25 22:55:36 -0500

    mp3 release!

---
+++ mp3/List-ListIterator.hpp
@@ -0,0 +1,51 @@
+
+class ListIterator : public std::iterator<std::bidirectional_iterator_tag, T> {
+  private:
+    ListNode* position_;
+
+  public:
+    ListIterator() : position_(nullptr) { }
+    ListIterator(ListNode* x) : position_(x) { }
+
+    // Pre-Increment, ++iter
+    ListIterator& operator++() {
+        // @TODO: graded in MP3.1
+        return *this;
+    }
+    
+    // Post-Increment, iter++
+    ListIterator operator++(int) {
+        ListNode* temp = position_;
+        position_ = position_->next;
+        return ListIterator(temp);
+    }
+
+    // Pre-Decrement, --iter
+    ListIterator& operator--() {
+        position_ = position_->prev;
+        return *this;
+    }
+
+    // Post-Decrement, iter--
+    ListIterator operator--(int) {
+        // @TODO: graded in MP3.1
+        return ListIterator();
+    }
+
+    bool operator==(const ListIterator& rhs) {
+        return !(*this != rhs);
+    }
+
+    bool operator!=(const ListIterator& rhs) {
+        // @TODO: graded in MP3.1
+        return false;
+    }
+
+    const T& operator*() {
+        return position_->data;
+    }
+
+    const T* operator->() {
+        return &(position_->data);
+    }
+};
