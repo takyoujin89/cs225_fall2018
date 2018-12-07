e2818daaec946a808d2829ee704b5135d9f8b476
Wade Fagen-Ulmschneider <waf@illinois.edu>
2018-12-07 10:21:20 -0600

    MP7: Part 3 Release

---
+++ mp7/Graph2.hpp
@@ -0,0 +1,25 @@
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <list>
+
+/**
+ * Returns an std::list of vertex keys that creates some shortest path between `start` and `end`.
+ *
+ * This list MUST include the key of the `start` vertex as the first vertex in the list, the key of
+ * the `end` vertex as the last element in the list, and an ordered list of all vertices that must
+ * be traveled along the shortest path.
+ * 
+ * For example, the path a -> c -> e returns a list with three elements: "a", "c", "e".
+ * 
+ * @param start The key for the starting vertex.
+ * @param end   The key for the ending vertex.
+ */
+template <class V, class E>
+std::list<std::string> Graph<V,E>::shortestPath(const std::string start, const std::string end) {
+  std::list<std::string> path;
+
+
+  return path;
+}
+
